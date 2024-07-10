767ed89593ef782b12b075d921ecafc60829dfa3
Alvaro Santiago Medina <alvaro.medina@mi.unc.edu.ar>
2024-07-10 12:59:15 -0300

    More changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+
+
+// Redirigir a una nueva pestaña
+document.querySelectorAll('.icons a').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const url = this.href;
+        setTimeout(() => {
+            window.open(url, '_blank');
+        }, 300); // Tiempo en milisegundos para esperar antes de redirigir
+    });
+});
+
+//Desplegar menú de navegación
 document.addEventListener("DOMContentLoaded", function() {
     const toggleBtn = document.querySelector('.toggle___btn');
     const toggleBtnIcon = document.querySelector('.toggle___btn i');
